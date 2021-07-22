dc7a2a2a4afe01d21fdca864cd64d1e6b405a2e0
jackline <cheptanuiJackline@gmail.comgit config --global user.email cheptanuiJackline@gmail.com>
2021-07-22 22:10:17 +0300

    first commit

---
+++ jack.py
@@ -0,0 +1,35 @@
+ name=input("Enter your name: ")
+print(name*100)
+
+name =input("Enter your name : ")
+while 1:
+       print(name, end = " ")
+  
+       name = input("Enter your name : ")
+       for k in range (1, 101):
+        print(k, name)
+      
+        print("Squares of integers are : ")
+        for p in range (1, 21):
+             print(p,"->",p*p)
+
+for d in range(8, 90, 3):
+           print(d)
+
+for p in range (100,0,-2):
+  print(p,",",end=" ")
+  person=input("Enter your name:")  
+  w=int(input("Enter how many time you want to print"))
+  for i in range (w):
+                   print(person)
+
+
+
+
+    
+
+
+    
+
+     
+
